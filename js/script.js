cd69e45c37d15d1fa88be197fc8a88fea6c9b6f9
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-04-29 09:27:15 -0300

    Inicialização do JS com defer e as 4 formas mágicas.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+// console.log("Olá Mundo de verdade!");
+//Comentário simples, não executa nada no código
+/* Comentário  
+Múltiplo, também não executa nada  
+no código*/
+
+//Captura de um elemento e atrelar a ele um evento click.
+// const elementoH1 = document.getElementById("titulo");
+// // elementoH1.addEventListener("evento","função anônima");
+// elementoH1.addEventListener("click", function(){
+//     console.log("Agora vitaminado!");
+// });
