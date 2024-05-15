c4ba86020f22f17ba1bed868ec268d1b4312d0b6
leoc7sar <lcrn1102@gmail.com>
2024-05-15 12:05:55 -0300

    Opcão de erro e sucesso com script.

---
+++ js/script-sucesso.js
@@ -0,0 +1,11 @@
+let usuario = JSON.parse(localStorage.getItem("obj-validado"));
+console.log(`O usuário ${usuario.nome} realizou o login com email ${usuario.emailUsuario}.`);
+
+const tituloUsuario = document.querySelector("#user-titulo");
+tituloUsuario.innerText = usuario.nome;
+
+const botaoLogout = document.querySelector("#user-logout");
+botaoLogout.addEventListener("click", () => {
+    localStorage.removeItem("obj-validado");
+    window.location.href = "index.html";
+});
